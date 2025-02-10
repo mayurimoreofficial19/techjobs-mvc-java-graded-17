367fcc6248b7774b499825ac8403f9643a1a8c5a
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-02-10 17:33:06 -0600

    Task-2 completed

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -3,6 +3,7 @@ package org.launchcode.techjobsmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
@@ -19,6 +20,11 @@ public class SearchController {
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
+    }
+
+    @PostMapping("search")
+    public String displaySearchResults(Model model) {
+
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
